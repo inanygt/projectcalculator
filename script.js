5ddca41dcca7264619363f0fe61c65dc4a76bb3c
inan yigit <inanygt@hotmail.com>
2022-08-20 14:26:15 +0200

    basic math operators

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1. basic math operators
+
+function add(a,b) {
+   return a + b;
+}
+
+function subtract(a,b) {
+   return a - b;
+}
+
+function multiply(a,b) {
+   return a * b;
+}
+
+function divide(a,b) {
+   return a/b ;
+}
+
+console.log(divide(12,4))
+
